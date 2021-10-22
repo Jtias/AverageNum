3357a801aaa2b93c77c12e17723efe254e4c425f
Junan Nashit Tias <60799707+Jtias@users.noreply.github.com>
2021-10-21 21:13:47 -0400

    Add files via upload

---
+++ JunanTiasAs5Q3.cpp
@@ -0,0 +1,79 @@
+/*
+Name : Junan Nashit Tias
+Student ID : 200424052
+
+JunanTiasAs5Q3.cpp
+
+Write a program that reads an unspecified number of scores and determines how many are above or
+equal to the average and how many are below the average.
+
+Step 1: Enter the scores in an array
+Step 2: Calculate the average 
+Step 3: Find the number of scores above or equal to the average and the number of scores below the average.
+Step 4: Show the average, number of scores above or equal to the average and numbers of scores blow the average.
+
+Major Variables:
+
+const int SIZE
+int i
+int a
+int b
+double numOfElements
+int input
+int sum
+double avg
+
+Progress limitation: N/A
+*/
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+const int SIZE = 100;
+
+int main()
+{
+	int array[SIZE];
+	int i = 0;
+	int a = 0;
+	int b = 0;
+	double numOfElements = 0;
+	int input = 0;
+	int sum=0;
+	double avg = 0;
+
+	cout << fixed << setprecision(1);
+
+	cout << "Enter a new score: ";
+	cin >> input;
+
+
+	while ((input > 0) && (i < SIZE))
+	{
+		numOfElements++;
+		array[i] = input;
+		i++;
+		sum = sum + input;
+		avg = (sum / numOfElements);
+		cout << "Enter a new score: ";
+		cin >> input;
+		
+	}
+	for (int count = 0; count < numOfElements; count++)
+	{
+		if (array[count] >= avg)
+		{
+			a++;
+		}
+		else if (array[count] < avg)
+		{
+			b++;
+		}
+	}
+
+	cout << "Average is: " << avg << endl;
+	cout << "Number of scores above or equal to the average " << a << endl;
+	cout << "Number of scores below the average " << b << endl;
+
+	return 0;
+}
